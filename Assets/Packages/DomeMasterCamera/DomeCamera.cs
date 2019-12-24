a6738865b5b612afd8923e1fac122b947fcc91c5
XJINE <xjine.andorelseif@gmail.com>
2019-12-24 11:23:14 +0900

    First commit.

---
+++ Assets/Packages/DomeMasterCamera/DomeCamera.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class DomeCamera : MonoBehaviour
+{
+    #region Field
+
+    public Vector3Int resolution   = new Vector3Int(1024, 1024, 0); // Z in 0, 16, 24.
+    public FilterMode filterMode   = FilterMode.Bilinear;
+    public int        antiAliasing = 2;
+
+    [SerializeField] protected Camera cameraTop;
+    [SerializeField] protected Camera cameraLeft;
+    [SerializeField] protected Camera cameraRight;
+    [SerializeField] protected Camera cameraBottom;
+
+    [SerializeField] protected Material materialTop;
+    [SerializeField] protected Material materialLeft;
+    [SerializeField] protected Material materialRight;
+    [SerializeField] protected Material materialBottom;
+
+    protected RenderTexture renderTextureTop;
+    protected RenderTexture renderTextureLeft;
+    protected RenderTexture renderTextureRight;
+    protected RenderTexture renderTextureBottom;
+
+    #endregion Field
+
+    #region Method
+
+    protected virtual void Awake()
+    {
+        UpdateTextureSettings();
+    }
+
+    [ContextMenu("UpdateTextureSettings")]
+    public void UpdateTextureSettings()
+    {
+        if (this.renderTextureTop != null)
+        {
+            this.renderTextureTop.Release();
+            this.renderTextureLeft.Release();
+            this.renderTextureRight.Release();
+            this.renderTextureBottom.Release();
+
+            DestroyImmediate(this.renderTextureTop);
+            DestroyImmediate(this.renderTextureLeft);
+            DestroyImmediate(this.renderTextureRight);
+            DestroyImmediate(this.renderTextureBottom);
+        }
+
+        this.renderTextureTop = new RenderTexture(this.resolution.x, this.resolution.y, this.resolution.z)
+        { 
+            filterMode       = this.filterMode,
+            antiAliasing     = this.antiAliasing,
+            hideFlags        = HideFlags.HideAndDontSave,
+            wrapMode         = TextureWrapMode.Clamp,
+            autoGenerateMips = false,
+            anisoLevel       = 0,
+        };
+
+        this.renderTextureLeft   = new RenderTexture(this.renderTextureTop.descriptor);
+        this.renderTextureRight  = new RenderTexture(this.renderTextureTop.descriptor);
+        this.renderTextureBottom = new RenderTexture(this.renderTextureTop.descriptor);
+
+        this.cameraTop.targetTexture    = this.renderTextureTop;
+        this.cameraLeft.targetTexture   = this.renderTextureLeft;
+        this.cameraRight.targetTexture  = this.renderTextureRight;
+        this.cameraBottom.targetTexture = this.renderTextureBottom;
+
+        this.materialTop.mainTexture    = this.renderTextureTop;
+        this.materialLeft.mainTexture   = this.renderTextureLeft;
+        this.materialRight.mainTexture  = this.renderTextureRight;
+        this.materialBottom.mainTexture = this.renderTextureBottom;
+    }
+
+    #endregion Method
+}
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Unity_DomeMasterCamera
+
+<img src="https://github.com/XJINE/Unity_DomeMasterCamera/blob/master/Screenshot.png" width="100%" height="auto" />
+
+DomeMasterCamera makes DomeMaster image with smallest camera sets, without Cubemap.
+So the performance is more better than [DomeMaster image effect](https://github.com/XJINE/Unity_DomeMaster).
+
+## Import to Your Project
+
+You can import this asset from UnityPackage.
+
+- [DomeMasterCamera.unitypackage](https://github.com/XJINE/Unity_DomeMasterCamera/blob/master/DomeMasterCamera.unitypackage)
+
+## How to Use
+
+ 1. Add ``DomeCamera`` into your scene and it will render 4-way textures into each material.
+ 2. Add ``DomeMasterCamera`` into your scene. It has 4 meshes to make a DomeMaster image.
+ 3. ``DomeMasterCamera`` has also a camera, it will capture the 4 meshes. The result is DomeMaster image.
+
+Layer settings are important. Set some layer and culling mask settings to hide a DomeMasterMesh.
+
+## Difference
+
+There are some differences from [DomeMaster image effect](https://github.com/XJINE/Unity_DomeMaster).
+
+- DomeMasterCamera cannot change the FOV.
+- DomeMasterCamera needs low resolution image than DomeMaster effect.
+    - DomeMasterCamera makes 4 images to make a DomeMaster image.
+    - DomeMaster effect makes 5 images + 1 blank image to make a DomeMaster(Cubemap) image.
+- DomeMasterCamera is able to use any resolution.
+    - DomeMaster effect must use the Power-of-2 resolution image to make a Cubemap image. 
+- DomeMaster effect is more simple than DomeMasterCamera.
+
+### Performance
+
+DomeMasterCamera is about x1.5 faster than DomeMaster effect in popular cases.
+Because DomeMaster effect needs to render 6 images (5 + 1 blank image), and bend the images with FragmentShader.
+So the effect takes more cost when making a more high resolution image.
+
+## Reference
+
+- [iDome: Immersive gaming with the Unity game engine](http://paulbourke.net/papers/cgat09b/) : Original Paper.
+- [Creating interactive dome contents with Unity](https://www.orihalcon.co.jp/news/unity-dome-kit/)
